c29e4050c0689923a8e75d8a51fde58c40f5860e
Jakub Pala <pala.kuba99@gmail.com>
2022-07-27 16:00:44 +0200

    adding datepicker in diary

--- src/Components/Diary.js
+++ src/Components/Diary.js
@@ -1,106 +1,106 @@
-import React from 'react'
-import { DiaryStyled } from './Styles/DiaryStyled'
-import { Container } from './Styles/Container'
-import { Box } from './Styles/Box'
-import { Text } from './Styles/Global'
-import { database, auth } from '../Helpers/FirebaseConfig'
+import React from "react";
+import { DiaryStyled } from "./Styles/DiaryStyled";
+import { Container } from "./Styles/Container";
+import { Box } from "./Styles/Box";
+import { Text } from "./Styles/Global";
+import { database, auth } from "../Helpers/FirebaseConfig";
 import { ref, onValue } from "firebase/database";
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { currentDate } from '../Helpers/CurrentDate'
-
+import { currentDate } from "../Helpers/CurrentDate";
 export default function Diary(props) {
-
-  const [date, setDate] = useState(currentDate());
+  const [choosedDate, setChoosedDate] = useState(currentDate());
+  const [date, setDate] = useState(new Date());
   const [todayMealsList, setTodayMealsList] = useState([]);
 
+  useEffect(() => {
+    const currentDateMeals = ref(
+      database,
+      `users/${auth.currentUser.uid}/meals/${choosedDate}`
+    );
 
-  
-
-  useEffect(()=>{
-    const currentDateMeals = ref(database, `users/${auth.currentUser.uid}/meals/${date}`);
-
-    if(todayMealsList.length === 0){
-      onValue(currentDateMeals, (snapshot) => {
+    onValue(currentDateMeals, (snapshot) => {
+      if (snapshot.val() !== null || undefined) {
         const data = Object.entries(snapshot.val());
-        setTodayMealsList(data)
+        setTodayMealsList(data);
+      } else if (snapshot.val() == null || undefined) {
+        setTodayMealsList([]);
 
-      })
-    } 
-    console.log(date)
-  }, [date])
+      }
+    });
+
+  }, [choosedDate]);
 
   const count = (meal) => {
-    
-    return  meal.reduce((totalKcal, element) => {
-        return Math.floor(totalKcal + element.product_kcalInServing);
-      }, 0);
-  };
-
-  useEffect(()=> {
-    console.log(todayMealsList)
-
-  },[todayMealsList])
-
+    return meal.reduce((totalKcal, element) => {
+      return Math.floor(totalKcal + element.product_kcalInServing);
+    }, 0); }
 
   return (
-  <>
- <DatePicker selected={new Date()}
-  dateFormat="dd-MM-yyyy"
-  onChange={(date) => setDate(date)
-} />
+    <>
+      <DiaryStyled>
+        <DatePicker
+          className="date-picker"
+          selected={date}
+          dateFormat="dd-MM-yyyy"
+          onChange={(date) => {
+            setDate(date);
+            setChoosedDate(
+              `${date.getDate()}-${
+                date.getMonth() + 1 < 10
+                  ? `0${date.getMonth() + 1}`
+                  : `${date.getMonth() + 1}`
+              }-${date.getFullYear()}`
+            );
+          }}
+        ></DatePicker>
 
-    <DiaryStyled>
-        <Box>
-  
-          <h2>
-            
-          </h2>
-        </Box>
-        { todayMealsList.length > 0 &&
-
-        todayMealsList.map((element, i) => {
-          return (
-            <Box className='meals-container'>
-              <Text className='meal-number'>Meal {i +1}</Text>
-              <Box className='meal-box'>
-              {
-                                     element[1].map(meal => {
-                                      return (
-                     
-                                        <table>
-                                            <thead>
-                                              <tr>
-                                                <th>{meal.product_name}</th>
-                                              </tr>
-                                            </thead>
-                                            <tbody>
-                                              <tr>
-                                                <td><Text>serving:{meal.product_serving * 100}<strong>G</strong></Text></td>
-                                                <td><Text>kcal: <strong>{meal.product_kcalInServing}</strong></Text></td>
-                                              </tr>
-                                            </tbody>
-                                          </table>
-                                    
-                                        )
-                                    })
-              }
-              
+        {todayMealsList.length > 0 &&
+          todayMealsList.map((element, i) => {
+            return (
+              <Box className="meals-container">
+                <Text className="meal-number">Meal {i + 1}</Text>
+                <Box className="meal-box">
+                  {element[1].map((meal) => {
+                    return (
+                      <table>
+                        <thead>
+                          <tr>
+                            <th>{meal.product_name}</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>
+                              <Text>
+                                serving:{meal.product_serving * 100}
+                                <strong>G</strong>
+                              </Text>
+                            </td>
+                            <td>
+                              <Text>
+                                kcal:
+                                <strong>{meal.product_kcalInServing}</strong>
+                              </Text>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    );
+                  })}
+                </Box>
+                <Text className="meal-summary">
+                  TOTAL KCAL IN MEAL: <strong>{count(element[1])}</strong>
+                </Text>
               </Box>
-              <Text className='meal-summary'>TOTAL KCAL IN MEAL: <strong>{count(element[1])}</strong></Text>
-
-            </Box>
-            )
-          
-        })}
-        
-    </DiaryStyled>
+            );
+          })}
+          <Box className="summary-container">
+            <h3>Summary of the day</h3>
+            <Text>You've eated kcal this day</Text>
+          </Box>
+      </DiaryStyled>
     </>
-  )
+  );
 }
-
-
-
- 
-    
