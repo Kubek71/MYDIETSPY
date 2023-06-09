dcbb1919d52c1597018a1482d1ed3f174cea00e9
Jakub Pala <pala.kuba99@gmail.com>
2023-06-09 15:35:39 +0200

    adding env

--- src/Helpers/FirebaseConfig.js
+++ src/Helpers/FirebaseConfig.js
@@ -4,22 +4,20 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 
-
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAiRPXJohk_mK5QhtJbryAIc93wG9zpGM8",
-  authDomain: "react-mydietspy.firebaseapp.com",
-  projectId: "react-mydietspy",
-  storageBucket: "react-mydietspy.appspot.com",
-  messagingSenderId: "829063713066",
-  appId: "1:829063713066:web:ea31548cc28e3f7cfa4e53",
-  measurementId: "G-1BJQMB2BNJ",
-  databaseURL: "https://react-mydietspy-default-rtdb.europe-west1.firebasedatabase.app"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
+  databaseURL: process.env.REACT_APP_databaseURL,
 };
 
 // Initialize Firebase
